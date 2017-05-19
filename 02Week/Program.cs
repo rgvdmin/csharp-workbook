753b3a1d89b66dc2738508e6af932be7cbbb5a40
Rob Galvan <robgalvan@gmail.com>
2017-05-18 20:16:53 -0500

    pig latin assignment

---
+++ 02Week/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            PigLatin latin = new PigLatin();
+            latin.Run();
+        }
+    }
+}
